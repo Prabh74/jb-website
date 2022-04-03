397e5e6e6e6ec8d6b7738ba5de5ceabe605dd01e
Prabh74 <81363833+Prabh74@users.noreply.github.com>
2022-04-03 08:07:13 +0530

    animation once page loads

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import About from "./components/about";
 import Facilities from "./components/facilities/facilities";
 import Landing from "./components/landing";
 import OtherFacilities from "./components/otherFacilities";
 
 export default function App(){
+    const [pageLoaded, setPageloaded] = useState(false);
+
+    useEffect(() =>{
+        setPageloaded(true)
+    }, [])
     return(
+        pageLoaded && (
         <React.Fragment>
             <Landing />
             <About />
             <Facilities />
             <OtherFacilities />
-        </React.Fragment>
+        </React.Fragment>)
     )
 }
